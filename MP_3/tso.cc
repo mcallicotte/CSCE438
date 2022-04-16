a37bb45610f58a577a2aa9b355496a978425e24f
mcallicotte <callicotte2001@gmail.com>
2022-04-15 22:28:41 -0500

    added some background structures to coordinator

--- MP_3/tso.cc
+++ MP_3/tso.cc
@@ -10,18 +10,73 @@
 #include <string>
 #include <stdlib.h>
 #include <unistd.h>
+#include <vector>
 #include <google/protobuf/util/time_util.h>
 #include <grpc++/grpc++.h>
 
-#include "sns.grpc.pb.h"
+#include "coord.grpc.pb.h"
 
-void serverService() {  /*unimplemented*/ }
+using google::protobuf::Timestamp;
+using google::protobuf::Duration;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using csce438::Message;
+using csce438::ListReply;
+using csce438::Request;
+using csce438::Reply;
+using csce438::SNSService;
 
-void clientService() { }
+enum Type {master, slave, sychronizer}
+
+struct Server {
+	int clusterNum;
+	Type type;
+	std::string port;
+	ServerReaderWriter<Heartbeat, Heartbeat>* stream = 0; 
+
+	Server(int num, Type theType, std::string thePort): clusterNum{num}, type{theType}, port{thePort} { }
+}
+
+struct Client {
+	std::string username; 
+	int clusterNum;
+
+	Client(std::string name): username{name} {
+		int convert = atoi(name);
+		clusterNum = (convert % 3);
+	}
+}
+
+struct Cluster {
+	std::vector<Client> clients;
+	Server master, slave, sychronizer;
+}
+
+std::vector<Cluster> clusters(3);
+
+class CoordServiceImpl final : public CoordService::Service {
+	Status Login(ServerContext* context, const Request* request, Reply* reply) override {
+
+	}
+}
 
 // runs the client facing and server facing coordinator services as different threads
 void runCoord(std::string port) {
-	
+	std::string coordAddress = "0.0.0.0:" + port;
+	SNSServiceImpl service;
+
+	ServerBuilding builder;
+	builder.AddListeningPort(coordAddress, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	std::cout << "Coordinator listening on " << coordAddress << std::endl;
+
+	server->Wait();
 }
 
 
